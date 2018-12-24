cee27167470fc24a147ded23fd3b66d87035f831
nanachi772 <noranko2539@gmail.com>
2018-12-24 23:32:17 +0000

    Xmas commit

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,21 @@
-require 'test_helper'
+require './test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | EarlyRise"
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | EarlyRise"
   end
-
 end
