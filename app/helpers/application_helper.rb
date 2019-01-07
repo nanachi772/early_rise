91859b1dbc47c647f0228d7e5c47a7ae98935e58
nanachi772 <noranko2539@gmail.com>
2019-01-07 01:05:52 +0000

    custom-helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページごとのタイトルを返す。
+  def full_title(page_title = '')
+    base_title = "EarlyRising"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,17 +5,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
+    assert_select "title", "EarlyRising"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | EarlyRise"
+    assert_select "title", "EarlyRising"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | EarlyRise"
+    assert_select "title", "EarlyRising"
   end
 end
